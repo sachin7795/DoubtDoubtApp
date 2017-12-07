cd7deaa9a3747f28fbe5d43d4d612af37679acb2
Sachin Singh <sachinx1000@gmail.com>
2017-12-07 22:24:04 +0530

    final commit

--- app.js
+++ app.js
@@ -42,13 +42,13 @@ app.use('/api', route);
 app.use(express.static(path.join(__dirname, '/public')));
 
 //index route
-app.get('/', (req, res)=>{
-    res.send('Invalid endpoint');
-});
+ app.get('/', (req, res)=>{
+     res.send('Invalid endpoint');
+ });
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+ app.get('*', (req, res)=>{
+     res.sendFile(path.join(__dirname, 'public/index.html'));
+ });
 
 // starting the server
 app.listen(port, ()=>{
